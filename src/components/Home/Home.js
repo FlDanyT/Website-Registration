9951f44cfc8df385ff43b8c4fe82e36f7fbb4ab5
FlDanyT <96938294+FlDanyT@users.noreply.github.com>
2023-05-08 18:47:08 +0300

    Add files via upload

---
+++ src/components/Home/Home.js
@@ -0,0 +1,18 @@
+import React, {useContext} from "react";
+
+import Card from "../UI/Card/Card";
+import styles from "./Home.module.css";
+import Button from "../UI/Button/Button";
+import AuthContext from "../../store/auth-context";
+
+const Home = (props) => {
+  const ctx = useContext(AuthContext)
+  return (
+    <Card className={styles.home}>
+      <h1>Рады Вас Видеть Снова!</h1>
+      <Button onClick={ctx.onLogout}>Выход</Button>
+    </Card>
+  );
+};
+
+export default Home;
